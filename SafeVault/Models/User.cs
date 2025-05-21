89b71d33195da0b3b057a6cd57b05caab305ab6f
77menzs <77menzs@gmail.com>
2025-05-21 09:30:03 +0200

    Add user model

---
+++ SafeVault/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SafeVault;
+
+public class User
+{
+    public string Id { get; set; }
+    [Required]
+    public string Username { get; set; } // Fixed typo
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+}
